5811766300b6bead6d71c978e3370dae02d8daa7
Darya van Tienhoven <daryaheleen@gmail.com>
2021-02-15 23:12:14 +0100

    AoC 2019 dag 2, part one compleet

--- AoC 2019/02december/puzzle.py
+++ AoC 2019/02december/puzzle.py
@@ -1,1 +1,20 @@
 from input import int_code
+
+
+#part one:
+
+def find_start_code():
+    for i, x in enumerate(int_code):
+        if i % 4 == 0:
+            if x == 1:
+                int_code[int_code[i+3]] = (int_code[int_code[i+2]] +int_code[int_code[i+1]])
+            if x == 2:
+                int_code[int_code[i+3]] = (int_code[int_code[i+2]] * int_code[int_code[i+1]])
+            if x == 99:
+                return int_code[0]
+
+print(find_start_code())
+
+
+#part two:
+
