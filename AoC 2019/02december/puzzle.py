f0e8308aee158cfa1e70c9f1a0f67932736ec6d1
Darya van Tienhoven <daryaheleen@gmail.com>
2021-02-16 10:51:20 +0100

    2019 dag 2 deel 2

--- AoC 2019/02december/puzzle.py
+++ AoC 2019/02december/puzzle.py
@@ -3,18 +3,28 @@ from input import int_code
 
 #part one:
 
-def find_start_code():
-    for i, x in enumerate(int_code):
+def find_start_code(num_list):
+    for i, x in enumerate(num_list):
         if i % 4 == 0:
             if x == 1:
-                int_code[int_code[i+3]] = (int_code[int_code[i+2]] +int_code[int_code[i+1]])
+                num_list[num_list[i+3]] = (num_list[num_list[i+2]] + num_list[num_list[i+1]])
             if x == 2:
-                int_code[int_code[i+3]] = (int_code[int_code[i+2]] * int_code[int_code[i+1]])
+                num_list[num_list[i+3]] = (num_list[num_list[i+2]] * num_list[num_list[i+1]])
             if x == 99:
-                return int_code[0]
+                return num_list[0]
 
-print(find_start_code())
+#print(find_start_code(int_code))
 
 
-#part two:
+def find_inputs(num_list):
+    list_one = range(0, 100)
+    list_two = range(0, 100)
+    for i in list_one:
+        for j in list_two:
+            work_list = num_list.copy()
+            work_list[1] = i
+            work_list[2] = j
+            if find_start_code(work_list) == 19690720:
+                return i * 100 + j
 
+print(find_inputs(int_code))
