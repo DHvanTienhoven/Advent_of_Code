cdda461f35b0b79427376c29716b6bd67e084326
Darya van Tienhoven <daryaheleen@gmail.com>
2021-02-16 16:11:26 +0100

    dag 3 puzzel 2 af

--- AoC 2019/03december/puzzle.py
+++ AoC 2019/03december/puzzle.py
@@ -29,7 +29,7 @@ def path_cable(cable):
 
 def find_intersections(cable_1, cable_2):
     set_one, set_two = set(path_cable(cable_1)), set(path_cable(cable_2))
-    return list(set_one &set_two)
+    return list(set_one & set_two)
 
 def find_smallest_manhattan_distance(cable_1, cable_2):
     l = find_intersections(cable_1, cable_2)
@@ -41,3 +41,50 @@ def find_smallest_manhattan_distance(cable_1, cable_2):
     return min(manhattan_distances)
 
 print(find_smallest_manhattan_distance(cable_one, cable_two))
+
+#part 2:
+
+intersections = find_intersections(cable_one, cable_two)
+
+def find_number_of_steps(cable, intersection):
+    num_steps = 0
+    lr_direction = 0
+    ud_direction = 0
+    for dir in cable:
+        num_steps += -1
+        steps = range(0, int(dir[1:])+1)
+        if dir[0] == 'R':
+            for step in steps:
+                num_steps += 1
+                if f'{lr_direction + step}, {ud_direction}' == intersection:
+                    return num_steps
+            lr_direction += int(dir[1:])
+        if dir[0] == 'U':
+            for step in steps:
+                num_steps += 1
+                if f'{lr_direction}, {ud_direction + step}' ==intersection:
+                    return num_steps
+            ud_direction += int(dir[1:])
+        if dir[0] == 'D':
+            for step in steps:
+                num_steps += 1
+                if f'{lr_direction}, {ud_direction -step}' == intersection:
+                    return num_steps
+            ud_direction += -int(dir[1:])
+        if dir[0] == 'L':
+            for step in steps:
+                num_steps += 1
+                if f'{lr_direction -step}, {ud_direction}' == intersection:
+                    return num_steps
+            lr_direction += -int(dir[1:])
+
+def find_lowest_sum_of_steps(cable_1, cable_2, interscection_list):
+    sums_of_steps = []
+    for inters in interscection_list:
+        sums_of_steps.append(find_number_of_steps(cable_1, inters) + find_number_of_steps(cable_2, inters))
+    sums_of_steps.remove(0)
+    return min(sums_of_steps)
+
+print(find_lowest_sum_of_steps(cable_one, cable_two, intersections))
+
+#right answers but will attempt some refactoring another time
